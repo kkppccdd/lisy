f817aa45125addd98065ec29978409af91ab9e01
kkppccdd <kkppccdd@gmail.com>
2012-12-02 15:53:04 +0800

    CLOSED - task 2: Kkppccdd http://github.com/kkppccdd/lisy/pull/2

---
+++ game/src/test/java/me/lisy/game/connector/NativeAppConnectorTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package me.lisy.game.connector;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author kkppccdd
+ * @email kkppccdd@gmail.com
+ * @date Dec 2, 2012
+ *
+ */
+public class NativeAppConnectorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
